53f7ebd63036f307c5885043cb9093a655c34b8d
Pranav Pore <pranav@pranavpore.com>
2023-06-08 17:41:52 +0530

    updated code

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -1,14 +1,17 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import avatar from '../assets/profile.png'
 import styles from '../styles/Username.module.css'
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import {useFormik} from 'formik';
 import { passwordValidate } from "../helper/validate";
 import useFetch from "../hooks/fetch.hook";
 import { useAuthStore } from "../store/store";
+import { verifyPassword } from "../helper/helper";
+
 
 export default function Password() {
+    const navigate = useNavigate();
     const { username } = useAuthStore(state => state.auth)
     const [{ isLoading, apiData, serverError }] = useFetch(`/user/${username}`);
     const formik = useFormik({
@@ -19,7 +22,17 @@ export default function Password() {
         validateOnBlur: false,
         validateOnChange: false,
         onSubmit: async values => {
-            console.log(values)
+            let loginPromise = verifyPassword({ username, password: values.password });
+            toast.promise(loginPromise, {
+                loading: 'Checking...',
+                success: <b>Logged in successfully</b>,
+                error: <b>Password incorrect</b>
+            });
+            loginPromise.then(res => {
+                let { token } = res.data;
+                localStorage.setItem('token', token);
+                navigate('/profile');
+            })
         }
     });
 
