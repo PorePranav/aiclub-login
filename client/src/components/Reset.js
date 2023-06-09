4c905bb38b03dc3448eccf7beede2add4c231765
Pranav Pore <pranav@pranavpore.com>
2023-06-09 11:03:23 +0530

    fixed warnings

--- client/src/components/Reset.js
+++ client/src/components/Reset.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import toast, { Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
 import { resetPasswordValidate } from '../helper/validate'
@@ -13,7 +13,7 @@ export default function Reset() {
 
   const { username } = useAuthStore(state => state.auth);
   const navigate = useNavigate();
-  const [{ isLoading, apiData, status, serverError }] = useFetch('createResetSession')
+  const [{ isLoading, status, serverError }] = useFetch('createResetSession')
 
   const formik = useFormik({
     initialValues : {
